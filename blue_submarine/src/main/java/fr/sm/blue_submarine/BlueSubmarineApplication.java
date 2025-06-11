3885a4bb73b0e65764eff86a4793974c614635ac
Sebastien Martagex <sma@ad.vif.fr>
2025-06-11 11:05:39 +0200

    Push initial

---
+++ blue_submarine/src/main/java/fr/sm/blue_submarine/BlueSubmarineApplication.java
@@ -0,0 +1,13 @@
+package fr.sm.blue_submarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSubmarineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSubmarineApplication.class, args);
+	}
+
+}
---
+++ blue_submarine/src/test/java/fr/sm/blue_submarine/BlueSubmarineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sm.blue_submarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueSubmarineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
